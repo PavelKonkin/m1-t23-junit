394bed121260097ad1a23267feb9ff20ce10fd62
Pavel Konkin <idmfani@gmail.com>
2023-11-19 12:19:01 +0300

    Feature: add tests for RPN

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,37 @@
 package notation;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReversePolishNotationCalculatorTest {
-
+    ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
     @Test
     public void shouldCalculateAddition() {
+        int result = calculator.calculatePolishNotation("2 3 +");
+        assertEquals(5, result);
+    }
 
+    @Test
+    public void shouldCalculateNegation(){
+        int result = calculator.calculatePolishNotation("2 3 -");
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void shouldCalculateMultiplication(){
+        int result = calculator.calculatePolishNotation("2 3 *");
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void shouldCalculateDataWithMultipleSpaces() {
+        int result = calculator.calculatePolishNotation("2     3      *  ");
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void shouldCalculateNegativeNumbers() {
+        int result = calculator.calculatePolishNotation("-2     3      *  ");
+        assertEquals(-6, result);
     }
 }
